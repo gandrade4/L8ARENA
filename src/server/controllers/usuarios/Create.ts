77e4828b13814b79b9bf2381b8d547dd7fc8b976
Ana Julia Araujo <142631671+ajlaraujo@users.noreply.github.com>
2024-08-11 13:51:06 -0300

    create user and save to JSON file

--- src/server/controllers/usuarios/Create.ts
+++ src/server/controllers/usuarios/Create.ts
@@ -1,14 +1,18 @@
-import { Request, Response } from 'express';
+import { Request, RequestHandler, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
+import fs from 'fs/promises'
+import path from 'path';
 
 
 interface User {
+    id?: number;
     nome: string;
     email: string;
     senha: string;
 }
 
+const dataPath = path.join(__dirname, '../../database/users.json');
 
 const bodyValidation: yup.Schema<User> = yup.object().shape({
     nome: yup.string().required().min(3),
@@ -16,36 +20,44 @@ const bodyValidation: yup.Schema<User> = yup.object().shape({
     senha: yup.string().required().min(8)
 });
 
-export const create = async (req: Request<{}, {}, {} , {}, User>, res: Response) => {
-    let validateData: User | undefined = undefined;
-
+export const createBodyValidator: RequestHandler = async (req, res, next) => {
     // Tenta validar os dados recebidos no corpo da requisição
     try {
-        validateData = await bodyValidation.validate(req.body, { abortEarly: false});  
-    } catch (error) {  // Se ocorrer um erro o código entrará no bloco `catch`
-        const yupError = error as yup.ValidationError;  // Convertendo o erro para ValidationError
-        const validationErrors: Record<string, string> = {};  // Criando um objeto para armazenar os erros de validação
+        await bodyValidation.validate(req.body, { abortEarly: false});  // abortEarly: Validando todos os erros primeiro
+        return next();  // Executa o próximo handler
+    } catch (err) {  // Se ocorrer um erro o código entrará no bloco `catch`
+        const yupError = err as yup.ValidationError;  // Convertendo o erro para ValidationError
+        const errors: Record<string, string> = {};  // Criando um objeto para armazenar os erros de validação
 
-        // Processa cada erro de requisição e adiciona ao objeto `validationErrors`
+        // Processa cada erro de requisição e adiciona ao objeto `errors`
         yupError.inner.forEach(error => {
             if (error.path === undefined) return;
-            validationErrors[error.path] = error.message;
-        })
+            errors[error.path] = error.message;
+        });
 
 
         // Envia uma resposta de erro para o cliente com detalhes dos problemas encontrados
-        return res.status(StatusCodes.BAD_REQUEST).json({
-            errors: validationErrors
-        })
+        return res.status(StatusCodes.BAD_REQUEST).json({ errors });
     }
-
-    console.log(validateData);
-
 };
 
 
-/*export const create = async (req: Request<{}, {}, {} , {}, User>, res: Response) => {
-    console.log(req.body)
 
-    return res.send(StatusCodes.INTERNAL_SERVER_ERROR).send('Não Implementado');
-};*/
+
+
+
+export const create = async (req: Request<{}, {}, User>, res: Response) => {
+    console.log(req.body);
+
+    const data = await fs.readFile(dataPath, 'utf-8');  // Lendo o arquivo JSON que contém os dados dos usuários
+    const users:  User[] = JSON.parse(data);  // Transforma os dados lidos em um array de usuários
+    
+    const newUser: User = req.body;  // Cria um novo usuário apartir dos dados recebidos na requisição
+    newUser.id = users.length ? users[users.length - 1].id! + 1 : 1;  // ´users.length ? ... : ...´ é uma estrutura de decisão; condição ? valor_se_verdadeiro : valor_se_falso;
+    users.push(newUser);  // Adiciona novo usuário ao array 
+
+    await fs.writeFile(dataPath, JSON.stringify(users, null, 2));  // Converte o array de volta para uma string JSON e escreve no arquivo
+     
+    return res.send('Criando...');
+
+};
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -9,10 +9,14 @@ router.get('/', (_,res) => {
     return res.send('ola');
 });
 
+
 // Criando uma rota de requisições `POST` no caminho `/usuarios` 
 // `UserController.createValidation` valida os dados antes de passar para o controlador (middleware)
 // `UserController.create` processa a requisição e executa a lógica para criar um usuário
-router.post('/usuarios', /*UserController.createValidation,*/ UserController.create);
+router.post('/users', UserController.createBodyValidator, UserController.create);
+//router.get('/users', UserController.list);
+//router.put('/users/:id', UserController.createBodyValidator, UserController.update);
+//router.delete('/users/:id', UserController.remove);
 
 // Exportanto as regras de rota p/ serem utilizadas no servidor
 export { router }; 
