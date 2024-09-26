a66a354e44d17e37e68e1a72f0bce8e6f4a21e77
anabclau <142626135+anabclau@users.noreply.github.com>
2024-09-26 04:06:04 -0300

    ajustes no back end e add cors

--- projeto final/backend/src/index.ts
+++ projeto final/backend/src/index.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes';
 import quadraRoutes from './routes/quadraRoutes';
-import reservaRoutes from './routes/reservaRoutes';
+//import reservaRoutes from './routes/reservaRoutes'; comentei pois ainda nao tinha essa parte
 import authRoutes from './routes/authRoutes'
 import { AppDataSource } from './dataSource';
+import cors from 'cors'; 
 
 
 async function startServer() {
@@ -12,13 +13,18 @@ async function startServer() {
 
     const app = express();
     const port = 2222;
-    
+
+    app.use(cors({
+        origin: 'http://localhost:5173', 
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+        credentials: true
+    }));
     
     app.use(express.json());
     
     app.use('/users', userRoutes);
     app.use('/quadras', quadraRoutes);
-    app.use('/reservas', reservaRoutes);
+    //app.use('/reservas', reservaRoutes);
     app.use('/', authRoutes);
     
     app.listen(port, () => {
